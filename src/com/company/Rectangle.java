4c9da57335a903969c3093ee2db7b598f28de076
Dzmitry <zhdv27@mail.ru>
2020-11-02 11:34:40 +0300

    second commit

--- src/com/company/Rectangle.java
+++ src/com/company/Rectangle.java
@@ -3,14 +3,19 @@ package com.company;
 class Rectangle {
     double x;
     double y;
+    private static int createdRectangles;
+    private  final  static String RUSSIAN_CLASS_NAME = "Прямоугольник";
+    private  final  static String ENGLISH_CLASS_NAME = "Rectangle";
 
     public Rectangle(double x1, double y1) {
         x = x1;
         y = y1;
+        createdRectangles = createdRectangles +1;
     }
     public Rectangle(double z) {
         x = z;
         y = z;
+        createdRectangles = createdRectangles +1;
     }
     double calculateArea() {
         return x * y;
@@ -25,8 +30,20 @@ class Rectangle {
         { System.out.println(" это прямоугольник");}}
 
     boolean isTheSameRectangle(Rectangle rectangle) {
-        return  ((this.x ==(rectangle.x)) &&
-                (this.y == (rectangle.y)));
+        return  this.x ==rectangle.x &&
+                this.y == rectangle.y;}
+
+    static void printRectanglesCount()
+    {
+               System.out.println("Всего было создано " +createdRectangles + " прямоугольника" );
+    }
+
+    static void  printClassName(boolean printlnRussian) {
+
+        if (printlnRussian){
+            System.out.println(RUSSIAN_CLASS_NAME);}
+        else {
+            System.out.println(ENGLISH_CLASS_NAME);}
+
     }
 }
-
